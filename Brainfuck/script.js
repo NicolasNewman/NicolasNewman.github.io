a3369cdcded3d6aa3eac2977d57e8e65100850a9
Nicolas Newman <nnewman316@gmail.com>
2016-08-04 13:24:23 -0500

    Added a Brainfuck Interpreter
    v3.0

---
+++ Brainfuck/script.js
@@ -0,0 +1,77 @@
+var LENGTH = 65534;
+var cell = [0];
+var pointer = 0;
+var code = "";
+var output = "";
+var bPlace = 0;
+
+function run() {
+	console.clear();
+	pointer = 0;
+	output = "";
+	for(var x = 0; x < code.length; x++) {
+		cell[x] = 0;
+	}
+	code = document.getElementById('input').value; 
+	bPlace = 0;
+	for(var x = 0; x < code.length; x++) {
+		if(code.charAt(x) === "+") {
+			cell[pointer]++;
+		}
+		if(code.charAt(x) === "-") {
+			cell[pointer]--;
+		}
+		if(code.charAt(x) === ".") {
+			output = output + String.fromCharCode(cell[pointer]);
+			console.log("Setting letter to: " + String.fromCharCode(cell[pointer]))
+		}
+		if(code.charAt(x) === ",") {
+			
+		}
+		if(code.charAt(x) === ">") {
+			console.log("Pointer: " + pointer)
+			console.log("-------------------")
+			pointer++;
+			cell.push(0);
+		}
+		if(code.charAt(x) === "<") {
+			if(pointer != 0) {
+				console.log("Pointer: " + pointer)
+				console.log("-------------------")
+				pointer--;
+			}
+		}
+		if(code.charAt(x) === "[") {
+			if(cell[pointer] == 0) {
+				x++;
+				while(bPlace > 0 || code.charAt(x) != ']') {
+					if(code.charAt(x) === '[') {
+						bPlace++;
+					}
+					if(code.charAt(x) === ']') {
+						bPlace--;
+					}
+					x++;
+				}
+			}
+		}
+		if(code.charAt(x) === "]") {
+			if(cell[pointer] !== 0) {
+				x--;
+				while(bPlace > 0 || code.charAt(x) != '[') {
+					if(code.charAt(x) === ']') {
+						bPlace++;
+					}
+					if(code.charAt(x) === '[') {
+						bPlace--;
+					}
+					x--;
+				}
+				x--;
+			}
+		}
+		console.log("Cell Value: " + cell[pointer])
+	}
+	document.getElementById('output').value = output; 
+	console.log("Output: " + output)
+}
